baa9663eba6b6816185c5f9fc5d57ff747657822
chocologica1 <music.80.love@gmail.com>
2018-12-06 13:34:33 +0900

    first commit

--- src/com/internousdev/sunflower/action/ProductListAction.java
+++ src/com/internousdev/sunflower/action/ProductListAction.java
@@ -1,9 +1,29 @@
 package com.internousdev.sunflower.action;
 
-import org.apache.struts2.interceptor.SessionAware;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.internousdev.sunflower.dao.ProductInfoDAO;
+import com.internousdev.sunflower.dto.ProductInfoDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
-public class ProductListAction extends ActionSupport implements SessionAware {
+public class ProductListAction extends ActionSupport {
+	//フィールド
+	private List<ProductInfoDTO> productInfoDTOList = new ArrayList<ProductInfoDTO>();
 
+	//メソッド（商品一覧を取得するだけ）
+	public String execute() {
+		String result = ERROR;
+		ProductInfoDAO productInfoDAO = new ProductInfoDAO();
+		productInfoDTOList = productInfoDAO.getProductInfoList();
+		if (productInfoDTOList.size() != 0) {
+			result = SUCCESS;
+		}
+		return result;
+	}
+
+	//アクセサ
+	public List<ProductInfoDTO> getProductInfoDTOList() {
+		return productInfoDTOList;
+	}
 }
