be9cb689aa06e52ae5df203a3b3209cfde5b5297
yigc27 <yuki.nk27@gmail.com>
2018-12-06 12:05:39 +0900

    CartInfoDAO.java

--- src/com/internousdev/sunflower/dao/CartInfoDAO.java
+++ src/com/internousdev/sunflower/dao/CartInfoDAO.java
@@ -13,15 +13,16 @@ public class CartInfoDAO {
 
 	private DBConnector db = new DBConnector();
 
+
 	/**
 	 * カート内商品情報一覧を取得します。
 	 *
-	 * @param loginId	ユーザーID
+	 * @param userId	ユーザーID
 	 * @param tempUserId	仮ユーザーID(session内"tempUserId")
 	 * @return	カート情報一覧をDTOListで戻します
 	 */
 
-	public List<CartInfoDTO> getCartInfoDTOList(String loginId, String tempUserId) {
+	public List<CartInfoDTO> getCartInfoDTOList(String userId, String tempUserId) {
 		List<CartInfoDTO> cartInfoDTOList = new ArrayList<>();
 		Connection con = db.getConnection();
 		String sql = "SELECT "
@@ -41,14 +42,14 @@ public class CartInfoDAO {
 				+ "pi.image_file_name,"
 				+ "pi.release_date,"
 				+ "pi.release_company,"
-				+ "pi.status"
-				+ "FROM cart_info ci"
-				+ "LEFT JOIN product_info pi"
-				+ "ON ci.product_id = pi.product_id"
+				+ "pi.status "
+				+ "FROM cart_info ci "
+				+ "LEFT JOIN product_info pi "
+				+ "ON ci.product_id = pi.product_id "
 				+ "WHERE user_id = ? OR temp_user_id = ?";
 		try{
 			PreparedStatement ps = con.prepareStatement(sql);
-			ps.setString(1, loginId);
+			ps.setString(1, userId);
 			ps.setString(2, tempUserId);
 			ResultSet rs = ps.executeQuery();
 			while(rs.next()){
@@ -89,70 +90,58 @@ public class CartInfoDAO {
 	 * @param tempUserId	仮ユーザーID
 	 * @return	削除に成功した場合正の整数、失敗した場合は0を戻します
 	 */
-	public int delete(int productId, String loginId, String tempUserId) {
+	public int delete(int productId, String userId, String tempUserId) {
 		return 0;
 	}
 
 	/**
-	 * 未実装です。
-	 * カート内に商品を追加します。
+	 * カート内に商品を追加します。<br>
+	 * カート内に同じ商品が既に存在した場合(existsProductメソッドで判定)、該当商品の個数を変更します。(addCountメソッドの呼び出し)
 	 * @param productId	追加する商品ID
 	 * @param productCount	追加する個数
-	 * @param loginId	ユーザーID
+	 * @param userId	ユーザーID
 	 * @param tempUserId	仮ユーザーID
 	 * @return	追加に成功した場合正の整数、失敗した場合は0を戻します
 	 */
 
-	public int regist(int productId, int productCount, String loginId, String tempUserId) {
-		return 0;
+	public int regist(int productId, int productCount, String userId, String tempUserId) {
+		int result = 0;
+		if(existsProduct(productId,userId,tempUserId)){
+			result = addCount(productId,productCount,userId,tempUserId);
+		}else{
+			userId = userId == null ? tempUserId : userId;
+			Connection con = db.getConnection();
+			String sql = "INSERT INTO cart_info(product_id,product_count,user_id,temp_user_id,regist_date,update_date) VALUES(?,?,?,?,now(),now())";
+			try{
+				PreparedStatement ps = con.prepareStatement(sql);
+				ps.setInt(1, productId);
+				ps.setInt(2, productCount);
+				ps.setString(3, userId);
+				ps.setString(4, tempUserId);
+				result = ps.executeUpdate();
+			}catch(SQLException e){
+				e.printStackTrace();
+			}finally{
+				try{con.close();}catch(SQLException e){}
+			}
+		}
+		return result;
 	}
 
 	/**
-	 * 未実装です。
 	 * ログインIDと仮ログインIDで保持していたカート情報をリンクさせます。
 	 * @param tempUserId	仮ログインID
 	 * @param LoginId	ユーザID
 	 * @return	追加に成功した場合正の整数、失敗した場合は0を戻します
 	 */
-	public int linkToLoginId(String tempUserId,String loginId){
-		return 0;
-	}
-
-	/**
-	 * 未実装です。
-	 * カート内の全ての商品の合計金額を戻します。
-	 * @param tempUserId	仮ログインID
-	 * @param loginId	ユーザーID
-	 * @return	合計金額を戻します
-	 */
-	public int getTotalPrice(String tempUserId,String loginId){
-		return 0;
-	}
-
-	/**
-	 * 未実装です。
-	 * カート内に指定された商品があるかを調べます。
-	 * @param productId
-	 * @param loginId
-	 * @param tempUserId
-	 * @return	指定された商品があるかboolean値で戻します。
-	 */
-	public boolean existsProduct(int productId,String loginId,String tempUserId){
-		return false;
-	}
-
-	/**
-	 * カート内の全ての商品を削除します。
-	 * @param loginId	ユーザーID
-	 * @return	削除に成功した場合、削除件数（品目数）をintで戻します。削除されていない場合0を戻します。
-	 */
-	public int deleteAll(String loginId){
-		Connection con = db.getConnection();
-		String sql = "DELETE FROM cart_info WHERE loginId = ?";
+	public int linkToLoginId(String userId,String tempUserId){
 		int result = 0;
+		Connection con = db.getConnection();
+		String sql = "UPDATE cart_info SET login_id = ? WHERE temp_user_id = ?";
 		try{
 			PreparedStatement ps = con.prepareStatement(sql);
-			ps.setString(1, loginId);
+			ps.setString(1, userId);
+			ps.setString(2, tempUserId);
 			result = ps.executeUpdate();
 		}catch(SQLException e){
 			e.printStackTrace();
@@ -164,13 +153,84 @@ public class CartInfoDAO {
 
 	/**
 	 * 未実装です。
-	 * 既にカートに入ってある商品の個数を変更します。
+	 * カート内の全ての商品の合計金額を戻します。
+	 * @param tempUserId	仮ログインID
+	 * @param userId	ユーザーID
+	 * @return	合計金額を戻します
+	 */
+	public int getTotalPrice(String userId,String tempUserId){
+		return 0;
+	}
+
+	/**
+	 * カート内に指定された商品があるかを調べます。
+	 * @param productId
+	 * @param userId
+	 * @param tempUserId
+	 * @return	指定された商品があるかboolean値で戻します。
+	 */
+	public boolean existsProduct(int productId,String userId,String tempUserId){
+		int result = 0;
+		Connection con = db.getConnection();
+		String sql = "SELECT * FROM cart_info WHERE product_id = ? AND (user_id = ? OR temp_user_id = ?)";
+		try{
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setInt(1, productId);
+			ps.setString(2, userId);
+			ps.setString(3, tempUserId);
+			result = ps.executeUpdate();
+		}catch(SQLException e){
+			e.printStackTrace();
+		}finally{
+			try{con.close();}catch(SQLException e){}
+		}
+		return result > 0;
+	}
+
+	/**
+	 * カート内の全ての商品を削除します。
+	 * @param userId	ユーザーID
+	 * @return	削除に成功した場合、削除件数（品目数）をintで戻します。削除されていない場合0を戻します。
+	 */
+	public int deleteAll(String userId){
+		Connection con = db.getConnection();
+		String sql = "DELETE FROM cart_info WHERE user_id = ?";
+		int result = 0;
+		try{
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setString(1, userId);
+			result = ps.executeUpdate();
+		}catch(SQLException e){
+			e.printStackTrace();
+		}finally{
+			try{con.close();}catch(SQLException e){}
+		}
+		return result;
+	}
+
+	/**
+	 * 既にカートに入っている商品の個数を変更します。
 	 * @param productId	対象の商品ID
 	 * @param count	個数です。この数量をカート情報テーブルのproduct_countに足します。
-	 * @param loginId	ユーザーID
+	 * @param userId	ユーザーID
 	 * @param tempUserId	仮ユーザーID
 	 */
-	public void addCount(int productId,int count,String loginId,String tempUserId){
-
+	public int addCount(int productId,int productCount,String userId,String tempUserId){
+		int result = 0;
+		Connection con = db.getConnection();
+		String sql = "UPDATE cart_info SET product_count = ? , update_date = now() WHERE product_id = ? AND (user_id = ? OR temp_user_id = ?)";
+		try{
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setInt(1, productCount);
+			ps.setInt(2, productId);
+			ps.setString(3, userId);
+			ps.setString(4, tempUserId);
+			result = ps.executeUpdate();
+		}catch(SQLException e){
+			e.printStackTrace();
+		}finally{
+			try{con.close();}catch(SQLException e){}
+		}
+		return result;
 	}
 }
