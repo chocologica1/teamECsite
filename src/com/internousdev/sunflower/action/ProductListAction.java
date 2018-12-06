d780dbd6c7e97cbc2be25f60d91d065ba6506e95
chocologica1 <music.80.love@gmail.com>
2018-12-06 11:16:12 +0900

    dao修正

---
+++ src/com/internousdev/sunflower/action/ProductListAction.java
@@ -0,0 +1,7 @@
+package com.internousdev.sunflower.action;
+
+import org.apache.struts2.interceptor.SessionAware;
+
+public class ProductListAction extends ActionSUpport implements SessionAware {
+
+}
