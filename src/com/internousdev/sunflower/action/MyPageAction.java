f405cf0ae08e2fd8e81821f4566cd6d87de52911
REDsky96 <mmnnknm.lk@gmail.com>
2018-12-06 10:48:49 +0900

    12/6 10:48 MyPageAction mypage.jsp

--- src/com/internousdev/sunflower/action/MyPageAction.java
+++ src/com/internousdev/sunflower/action/MyPageAction.java
@@ -1,5 +1,49 @@
 package com.internousdev.sunflower.action;
 
-public class MyPageAction {
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class MyPageAction extends ActionSupport implements SessionAware {
+
+	private Map<String, Object> session;
+
+	public String execute(){
+
+		//loginedのsession有無を確認
+		if(!session.containsKey("logined")){
+          String result = "login";
+          return result;
+		}
+
+		UserInfoDAO userInfoDAO = new UserInfoDAO();
+		UserInfoDTO userInfoDTO = new UserInfoDTO();
+
+		userInfoDTO = userInfoDAO.getUserInfo(String.valueOf(session.get("logined")));
+
+		if(userInfoDAO != null){
+			session.put("familyName", userInfoDTO.getFamilyName());
+			session.put("firstName", userInfoDTO.getFirstName());
+			session.put("familyNameKana", userInfoDTO.getFamilyNameKana());
+			session.put("firstNameKana", userInfoDTO.getFirstNameKana());
+			session.put("sex", userInfoDTO.getSex());
+			session.put("email", userInfoDTO.getEmail());
+			//一応コンソールに表示させるために
+			System.out.println(session.get("familyName"));
+		}
+		return SUCCESS;
+	}
+
+	public Map<String, Object> getSession(){
+		return session;
+	}
+
+	public void setSession(Map<String, Object> session){
+		this.session = session;
+	}
+
+
 
 }
