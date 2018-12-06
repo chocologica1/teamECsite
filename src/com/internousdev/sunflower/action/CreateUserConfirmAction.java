30b34cb9415ed18ac60f9646322793e0e1767a0c
sneak9show <show9twll@gmail.com>
2018-12-06 10:34:22 +0900

    CreateUserConfirmAction

--- src/com/internousdev/sunflower/action/CreateUserConfirmAction.java
+++ src/com/internousdev/sunflower/action/CreateUserConfirmAction.java
@@ -10,6 +10,70 @@ import com.internousdev.sunflower.dao.UserInfoDAO;
 import com.internousdev.sunflower.util.InputChecker;
 import com.opensymphony.xwork2.ActionSupport;
 
-public class CreateUserConfirmAction extends ActionSupport implements {
+public class CreateUserConfirmAction extends ActionSupport implements SessionAware{
+
+	private String familyName;
+	private String firstName;
+	private String familyNameKana;
+	private String firstNameKana;
+	private String sex;
+	private String email;
+	private String loginId;
+	private String password;
+
+	private List<String> familyNameErrorMessage = new ArrayList<String>();
+	private List<String> firstNameErrorMessage = new ArrayList<String>();
+	private List<String> familyNameKanaErrorMessage = new ArrayList<String>();
+	private List<String> firstNameKanaErrorMessage = new ArrayList<String>();
+	private List<String> emailErrorMessage = new ArrayList<String>();
+	private List<String> loginIdErrorMessage = new ArrayList<String>();
+	private List<String> passwordErrorMessage = new ArrayList<String>();
+
+	private String categoryId;
+
+	public String execute(){
+
+		String result =ERROR;
+		InputChecker inputChecker =new InputChecker();
+
+		session.put("familyName",familyName);
+		session.put("firstName",firstName);
+		session.put("familyNameKana",familyNameKana);
+		session.put("firstNameKana",firstNameKana);
+		session.put("sex",sex);
+		session.put("email",email);
+		session.put("loginId",loginId);
+
+		familyNameErrorMessageList = inputChecker.doCheck("姓",familyName,1, 16, true, true, true, false, false, false, false, false, false);
+		firstNameErrorMessageList = inputChecker.doCheck("名", firstName, 1, 16, true, true, true, false, false, false, false, false, false);
+		familyNameKanaErrorMessageList = inputChecker.doCheck("姓ふりがな",familyNameKana,1, 16, false, false, true, false, false, false, false, false, false);
+		firstNameKanaErrorMessageList = inputChecker.doCheck("名ふりがな",firstNameKana,1, 16, false, false, true, false, false, false, false, false, false);
+		emailErrorMessageList = inputChecker.doCheck("メールアドレス", email, 10, 32, true, false, false, true, true, false, false, false, false);
+		loginIdErrorMessageList = inputChecker.doCheck("ユーザID", loginId, true, false, false, true, true, false, false, false, false, false, false);
+		passwordErrorMessageList = inputChecker.doCheck("パスワード", password, 1, 16, true, false, false, true, false, false, false, false, false);
+
+
+		if(familyNameErrorMessageList.size()==0
+				&& firstNameErrorMessageList.size()==0
+				&& familyNameKanaErrorMessageList.size()==0
+				&& firstNameKkanaErrorMessageList.size()==0
+				&& emailErrorMessageList.size()==0
+				&& loginIdErrorMessageList.size()==0
+				&& passwordErrorMessageList.size()==0) {
+			result = SUCCESS;
+		}else{
+			session.put("familyNameErrorMessageList", familyNameErrorMessageList);
+			session.put("firstNameErrorMessageList", firstNameErrorMessageList);
+			session.put("familyNameKanaErrorMessageList", familyNameKanaErrorMessageList);
+			session.put("firstNameKanaErrorMessageList", firstNameKanaErrorMessageList);
+			session.put("emailErrorMessageList", emailErrorMessageList);
+			session.put("loginIdErrorMessageList", loginIdErrorMessageList);
+			session.put("passwordErrorMessageList", passwordErrorMessageList);
+
+		}
+	}
+
+	}
+
 
 }
