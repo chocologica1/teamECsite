759ddb5b3011514f9106021eee72ed0f66669382
chocologica1 <music.80.love@gmail.com>
2018-12-06 14:07:34 +0900

    検索エラーのsession.remove追加

--- src/com/internousdev/sunflower/action/ProductListAction.java
+++ src/com/internousdev/sunflower/action/ProductListAction.java
@@ -2,18 +2,26 @@ package com.internousdev.sunflower.action;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
 
 import com.internousdev.sunflower.dao.ProductInfoDAO;
 import com.internousdev.sunflower.dto.ProductInfoDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
-public class ProductListAction extends ActionSupport {
+public class ProductListAction extends ActionSupport implements SessionAware {
 	//フィールド
 	private List<ProductInfoDTO> productInfoDTOList = new ArrayList<ProductInfoDTO>();
+	private Map<String,Object> session;
 
-	//メソッド（商品一覧を取得するだけ）
+	//メソッド
 	public String execute() {
 
+		//sessionに検索エラーが入っていたら切る
+		session.remove("keywordsErrorMessageList");
+
+		//商品一覧を取得
 		ProductInfoDAO productInfoDAO = new ProductInfoDAO();
 		productInfoDTOList = productInfoDAO.getProductInfoList();
 
@@ -24,4 +32,9 @@ public class ProductListAction extends ActionSupport {
 	public List<ProductInfoDTO> getProductInfoDTOList() {
 		return productInfoDTOList;
 	}
+	@Override
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
+	}
+
 }
