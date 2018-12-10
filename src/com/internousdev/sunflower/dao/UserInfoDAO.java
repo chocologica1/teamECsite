9372dc368dd11270968db37d5a0f61b8cf69ac05
yigc27 <yuki.nk27@gmail.com>
2018-12-10 14:46:11 +0900

    UserInfoDAO

--- src/com/internousdev/sunflower/dao/UserInfoDAO.java
+++ src/com/internousdev/sunflower/dao/UserInfoDAO.java
@@ -21,7 +21,7 @@ public class UserInfoDAO {
 		int count=0;
 
 		String sql ="insert into user_info(user_id,password,family_name,first_name,"
-				+ " family_name_kana,first_name_kana,sex,email,status,logined,regist_date,"
+				+ " family_name_kana,first_name_kana,sex,email,status,logined,regist_date,update_date"
 				+ ")"
 				+ " values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, now(), now())";
 
@@ -36,7 +36,7 @@ public class UserInfoDAO {
 			ps.setString(6, firstNameKana);
 			ps.setString(7, sex);
 			ps.setString(8, email);
-			ps.setInt(9,0);
+			ps.setInt(9, 0);
 			ps.setInt(10, 1);
 			count = ps.executeUpdate();
 		}catch(SQLException e){
